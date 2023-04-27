781493a0929b5ca550ce8a2b2ec48f44301ce518
rahul7r4 <127712688+rahul7r4@users.noreply.github.com>
2023-04-27 16:49:30 +0530

    Add files via upload

---
+++ Webcomponent.js
@@ -0,0 +1,39 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = 
+    `
+<input type="radio" name="option" value="BASE" checked>BASE
+<input type="radio" name="option" value="SCEN1">SCEN1
+<button type="button" id="myBtn" onclick="sendSelectedOption()">Commit Version</button>
+` ;   
+   
+    class PerformanceHelp extends HTMLElement {
+        constructor() {
+            super();
+            this.init();           
+        }
+
+        init() {            
+              
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+            var event = new Event("onClick");
+            this.fireChanged();           
+            this.dispatchEvent(event);
+            });           
+        }
+
+        fireChanged() {
+            console.log("OnClick Triggered");     
+            var selectedOption = document.querySelector('input[name="option"]:checked').value;
+		console.log("selectedOption"); 
+
+
+           
+        }        
+        
+    }
+
+    customElements.define('custom-button', PerformanceHelp);
+})();
