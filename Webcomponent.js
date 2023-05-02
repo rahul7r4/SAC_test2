0d365c3b307e1bc9216140ab507bfa87a93f76b3
rahul7r4 <127712688+rahul7r4@users.noreply.github.com>
2023-05-02 11:19:20 +0530

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -4,7 +4,7 @@
     `
 <input type="radio" name="option" value="BASE" checked>BASE
 <input type="radio" name="option" value="SCEN1">SCEN1
-<button type="button" id="myBtn">Commit Version</button>
+<button type="button" id="myBtn" onclick="getSelectedOption()>Commit Version</button>
 ` ;   
    
     class PerformanceHelp extends HTMLElement {
@@ -13,8 +13,7 @@
             this.init();           
         }
 
-        init() {            
-              
+        init() {              
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this.addEventListener("click", event => {
@@ -23,12 +22,51 @@
             this.dispatchEvent(event);
             });           
         }
-
-        fireChanged() {
+	   
+	fireChanged() {
             console.log("OnClick Triggered");     
             var selectedOption = document.querySelector('input[name="option"]:checked').value;
-		console.log("selectedOption"); }
-                 
+	    console.log("selectedOption"); }
+
+	function getSelectedOption() {
+	    console.log("Button Triggered");  
+	    // Get the selected radio button
+
+	    var selectedOption = document.querySelector('input[name="option"]:checked').value;
+
+	    // Display the selected option in the output paragraph
+	    document.getElementById("output").innerHTML = "Selected option: " + selectedOption;
+
+            // Set up the API endpoint URL
+	    const username = "sb-04a2113b-18e8-434e-ad82-38a9c9c53f02!b94776|it-rt-1f27c60btrial!b26655";
+	    const password = "0a68f493-2401-4a1a-8ce7-1c6b566c06e0$woMqw0mfMJoMns1CD8VDfOFABksxpiUTNJp1V4tR4kA= ";
+	    const auth = 'Basic' + btoa(username + ':' + password);
+            const apiUrl = 'https://1f27c60btrial.it-cpitrial05-rt.cfapps.us10-001.hana.ondemand.com/http/sac_to_s/4hana';
+
+            // Set up the request headers
+            const headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+
+            // Set up the request body data
+            const body = JSON.stringify({ selectedOption: selectedOption });
+
+            // Set up the request options
+            const options = {
+            method: 'POST',
+            headers: headers,
+            body: body,
+            };
+		console.log(options);
+		console.log(body);
+
+            // Send the API request
+            fetch(apiUrl, options)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error(error));		
+
+		}
+	                     
     }
 
     customElements.define('custom-button', PerformanceHelp);
