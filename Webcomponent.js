6be61807a33de57e1ba546fdb0b26301d3a47323
rahul7r4 <127712688+rahul7r4@users.noreply.github.com>
2023-05-02 16:15:54 +0530

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,14 +1,37 @@
-var selection = "BASE";
+var selectedOption = "BASE";
 
 function getSelectedOption(sel) {
     //console.log(sel.options[sel.selectedIndex].value);
     //var selectedOption = document.querySelector('input[name="option"]:checked').value;
-    selection = sel.options[sel.selectedIndex].value
-    console.log(selection); 
+    selectedOption = sel.options[sel.selectedIndex].value
+    console.log(selectedOption); 
 }
 
 function commitSelectedOption() {
-    console.log("Commit:" + selection); 
+    console.log("Commit:" + selectedOption); 
+    const username = "sb-04a2113b-18e8-434e-ad82-38a9c9c53f02!b94776|it-rt-1f27c60btrial!b26655";
+    const password = "0a68f493-2401-4a1a-8ce7-1c6b566c06e0$woMqw0mfMJoMns1CD8VDfOFABksxpiUTNJp1V4tR4kA= ";
+    const auth = 'Basic' + btoa(username + ':' + password);
+    const apiUrl = 'https://1f27c60btrial.it-cpitrial05-rt.cfapps.us10-001.hana.ondemand.com/http/sac_to_s/4hana';
+    // Set up the request headers
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    // Set up the request body data
+    const body = JSON.stringify({ Version: selectedOption });
+    // Set up the request options
+    const options = {
+    method: 'POST',
+    headers: headers,
+    body: body,
+    };
+    //console.log(options);
+    //console.log(body);
+    // Send the API request
+    fetch(apiUrl, options)
+	.then(response => response.json())
+	.then(data => console.log(data))
+	.catch(error => console.error(error));
+	
 }
 
 (function () {
@@ -39,7 +62,7 @@ function commitSelectedOption() {
         }
 
 	fireChanged() {
-            console.log("OnClick Triggered");
+            //console.log("OnClick Triggered");
 	    //const el = document.getElementById("myBtn");
 	    //console.log("el");
 	}
