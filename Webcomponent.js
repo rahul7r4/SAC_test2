4a447a9d666bb69b66f59361551d4ce7ec0592be
rahul7r4 <127712688+rahul7r4@users.noreply.github.com>
2023-05-02 12:22:31 +0530

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,17 +1,24 @@
+function getSelectedOption() {
+    console.log("Triggered");
+    var selectedOption = document.querySelector('input[name="option"]:checked').value;
+    console.log("selectedOption"); 
+}
+
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
     `
 <input type="radio" name="option" value="BASE" checked>BASE
 <input type="radio" name="option" value="SCEN1">SCEN1
-<button type="button" id="myBtn onclick="getSelectedOption()">Commit Version</button>
-<script>
-function getSelectedOption() {
-var selectedOption = document.querySelector('input[name="option"]:checked').value;
-console.log("selectedOption"); 
-}
-</script>
+<button type="button" id="myBtn" onclick="getSelectedOption()">Commit Version</button>
 ` ;   
+
+	const el = document.getElementById("myBtn");
+	el.addEventListener("click", event => {
+	        console.log("Triggered1")
+    		var selectedOption = document.querySelector('input[name="option"]:checked').value;
+   		console.log("selectedOption"); });
+
 
     class PerformanceHelp extends HTMLElement {
         constructor() {
